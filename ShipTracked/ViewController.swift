0da69fb0162138cea222fae76227c0eca91ba567
Thinh Luong <guitarslacker@gmail.com>
2016-05-16 15:20:40 -0500

    Initial Commit

---
+++ ShipTracked/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShipTracked
+//
+//  Created by Thinh Luong on 5/16/16.
+//  Copyright © 2016 Thinh Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
