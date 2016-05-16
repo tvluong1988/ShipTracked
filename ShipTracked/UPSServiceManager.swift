30db607e92521c180a7adc4910a07c51db1a38e6
Thinh Luong <guitarslacker@gmail.com>
2016-05-16 17:51:19 -0500

    added JSON data request functionality to UPSServiceManager

--- ShipTracked/UPSServiceManager.swift
+++ ShipTracked/UPSServiceManager.swift
@@ -18,9 +18,144 @@ class UPSServiceManager {
   }
   
   private func updateParcelInformationFromAPI(parcel: Parcel) -> Parcel {
+    sendDataTaskWithTrackingNumber(parcel.trackingNumber)
     return parcel.validateTrackingNumber()
+  }
+  
+  private func sendDataTaskWithTrackingNumber(trackingNumber: String) {
+    guard let body = createRequestBodyJSONWithTrackingNumber(trackingNumber),
+      let request = createNSURLRequestWithBody(body) else {
+        return
+    }
+    
+    sendDataTaskWithRequest(request)
+    
+  }
+  
+  private func sendDataTaskWithRequest(request: NSURLRequest) {
+    let task = session.dataTaskWithRequest(request) {
+      data, response, error in
+      
+      if let error = error {
+        print(error)
+        return
+      }
+      
+      do {
+        let result = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+        
+        print("Result from UPS json: \(result)")
+      } catch {
+        print(error)
+      }
+    }
+    
+    task.resume()
+  }
+  
+  private func createNSURLRequestWithBody(body: NSData) -> NSURLRequest? {
+    guard let url = NSURL(string: endpointURLTesting) else {
+      return nil
+    }
+    
+    let request = NSMutableURLRequest(URL: url)
+    request.HTTPMethod = "POST"
+    request.HTTPBody = body
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    
+    let staticRequest = request.copy() as? NSURLRequest
+    return staticRequest
+    
+  }
+  
+  private func createRequestBodyJSONWithTrackingNumber(trackingNumber: String) -> NSData? {
+    
+    let bodyDictionary: [String: AnyObject] =
+      [
+        "Security": [
+          "UsernameToken": [
+            "Username": username,
+            "Password": password
+          ],
+          "UPSServiceAccessToken": [
+            "AccessLicenseNumber": accessLicenseNumber
+          ]
+        ],
+        "TrackRequest": [
+          "Request": [
+            "RequestAction": "Track",
+            "RequestOption": requestOption
+          ],
+          "InquiryNumber": trackingNumber
+        ]
+    ]
+    
+    do {
+      let bodyJSON = try NSJSONSerialization.dataWithJSONObject(bodyDictionary, options: .PrettyPrinted)
+      print(bodyJSON)
+      return bodyJSON
+      
+    } catch {
+      print(error)
+      return nil
+    }
+    
   }
   
   // MARK: Properties
   
+  // JSON Production Endpoint
+  private let endpointURLProduction = "https://onlinetools.ups.com/json/Track"
+  
+  // JSON Testing Endpoint
+  private let endpointURLTesting = "https://wwwcie.ups.com/json/Track"
+  
+  private let username = "tvluong2"
+  private let password = "t7!zAWK^x1$6"
+  private let accessLicenseNumber = "DD0BE12EB3B46D36"
+  private let requestOption = "7"
+  
+  private let session = NSURLSession.sharedSession()
+  
+  let validTrackingNumberForTesting = "1Z202Y36A898759591"
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- ShipTracked/ViewController.swift
+++ ShipTracked/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    let _ = upsServiceManager.getParcelWithTrackingNumber(upsServiceManager.validTrackingNumberForTesting)
+    
   }
   
   override func didReceiveMemoryWarning() {
@@ -20,6 +23,7 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
-  
+  // MARK: Properties
+  let upsServiceManager = UPSServiceManager()
 }
 
