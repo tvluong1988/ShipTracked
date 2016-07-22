86e985c7f657085b47f7d55a2f5c9c7990af557c
Thinh Luong <guitarslacker@gmail.com>
2016-07-21 20:24:34 -0500

    test run

--- travis/script.sh
+++ travis/script.sh
@@ -5,7 +5,7 @@ fastlane ios build
 fastlane ios beta
 else
 open -b com.apple.iphonesimulator
-#fastlane ios test
-#fastlane ios report_test_coverage
+fastlane ios test
+fastlane ios report_test_coverage
 fastlane ios build
 fi
