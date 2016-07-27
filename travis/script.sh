e95b0a4150d61fe15118672531279a7a29c98988
Thinh Luong <guitarslacker@gmail.com>
2016-07-26 23:01:16 -0500

    test run

--- travis/script.sh
+++ travis/script.sh
@@ -4,8 +4,8 @@ if [ $TRAVIS_BRANCH = "master" ] && [ $TRAVIS_PULL_REQUEST == "false" ]; then
 fastlane ios build
 fastlane ios beta
 else
-#open -b com.apple.iphonesimulator
-#fastlane ios test
-#fastlane ios report_test_coverage
+open -b com.apple.iphonesimulator
+fastlane ios test
+fastlane ios report_test_coverage
 fastlane ios build
 fi
