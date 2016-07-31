515aa7a4a89af9055ae9e8f55a87ab211bc293f3
Thinh Luong <guitarslacker@gmail.com>
2016-07-30 23:54:01 -0500

    separate Item protocol to new file

--- ShipTracked/DataList.swift
+++ ShipTracked/DataList.swift
@@ -29,4 +29,3 @@ extension DataList {
   
 }
 
-protocol Item {}
---
+++ ShipTracked/Item.swift
@@ -0,0 +1,11 @@
+//
+//  Item.swift
+//  ShipTracked
+//
+//  Created by Thinh Luong on 7/30/16.
+//  Copyright © 2016 Thinh Luong. All rights reserved.
+//
+
+import Foundation
+
+protocol Item {}
